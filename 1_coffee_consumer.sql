568f9b26151d16a89ff6be151f2d7b0d8b103b42
subhrajyoutimahanta@gmail.com <subhrajyoutimahanta@gmail.com>
2025-02-06 14:00:53 +0530

    image

--- 1_coffee_consumer.sql
+++ 1_coffee_consumer.sql
@@ -1,1 +1,5 @@
-/*
+/*How many people in each city are estimated to consume coffee, given that 25% population does*/
+
+SELECT city_name, population,
+        population*0.25 as estimated_coffee_consume_people
+FROM city
---
+++ 2_total_revenue.sql
@@ -0,0 +1,19 @@
+--Total_revenue_across_all cities
+SELECT 
+        city.city_name,
+        sum(total) as Revenue
+
+FROM sales
+
+LEFT JOIN     
+    customers AS ct on sales.customer_id=ct.customer_id
+INNER JOIN    
+    city on ct.city_id=city.city_id
+WHERE 
+    EXTRACT(YEAR FROM sale_date)  = 2023
+	AND
+	EXTRACT(quarter FROM sale_date) = 4    
+GROUP BY
+    city.city_name    
+ORDER BY 
+    Revenue DESC    
